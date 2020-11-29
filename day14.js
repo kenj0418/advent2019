a2f0ab45d02cecf3d3e75f9b545e6bbc23aa6dcd
Ken Johnson <kenj0418@gmail.com>
2020-11-29 13:03:08 -0600

    Day 14 - Part 1 - Working with small test

--- day14.js
+++ day14.js
@@ -1,4 +1,4 @@
-const {readStringArrayFromFile, permutator} = require("./lib");
+const {readStringArrayFromFile} = require("./lib");
 
 const parseMaterial = (st) => {
   const parts = st.split(" ");
@@ -16,145 +16,62 @@ const parseReaction = (st) => {
   return {product, components};
 }
 
-const arrangeReactions = (reactions) => {
-  let arranged = {};
-
-  reactions.forEach((reaction) => {
-    const prod = reaction.product.material;
-    if (arranged[prod]) {
-      arranged[prod].push(reaction);
-    } else {
-      arranged[prod] = [reaction];
-    }
-  });
-
-  return arranged;
-}
-
-const useInventory = (inventory, needMaterial, needQty, level) => {
-  if (!inventory[needMaterial]) {
-    output(level, `No ${needMaterial} in inventory`);
-    return needQty
-  } else if (inventory[needMaterial] < needQty) {
-    const stillNeed = needQty - have;
-    delete inventory[needMaterial];
-    output(level, `Used all ${needMaterial} in inventory, still need ${stillNeed}`);
-    return stillNeed;
+const addToInventory = (inv, material, qty) => {
+  if (inv[material]) {
+    inv[material] += qty;
   } else {
-    inventory[needMaterial] -= needQty;
-    output(level, `Had enough ${needMaterial} in inventory, still have ${inventory[needMaterial]} left`);
-    return 0;
+    inv[material] = qty;
   }
+
+  // console.log(`inv[material]:${inv[material]}`);
+  return inv[material] >= 0
 }
-
-const combineInventory = (masterInv, addInv) => {
-  Object.getOwnPropertyNames(addInv).forEach((material) => {
-    if (masterInv[material]) {
-      masterInv[material] += addInv[material];
-    } else {
-      masterInv[material] = addInv[material];
-    }
-  })
-
-  console.log("INV TEST:" + JSON.stringify(addInv));
-}
-
-const output = (level, st) => {
-  if (level <= 0) {
-    console.log(st);
-  } else {
-    output(level - 1, "  " + st);
-  }
-}
-
-// const calculateMinimumOreReactions = (reactions, targetMaterial, targetQty, level) => {
-//   output(level, `Looking for ${targetQty} ${targetMaterial}`);
-
-//   if (targetMaterial == "ORE") {
-//     output(level, `BASE ORE: ${targetQty}`);
-//     return {oreNeeded: targetQty, reactions: [], left:{}}
-//   }
-
-//   const relevant = reactions[targetMaterial];
-//   if (!relevant || relevant.length == 0) {
-//     throw new Error(`Unable to produce ${targetMaterial}`);
-//   }
-
-//   let minOreReq = null
-//   let minOreReactions = null
-//   let minInv = null;
-
-//   relevant.forEach((react) => {
-//     let minTotal = 0;
-//     let minReactions = [];
-//     let inventory = {}
-
-    
-
-//     // trying every order since sometimes there will be leftovers that reduce the need to make more
-//     const componentPermutations = permutator(react.components);
-//     componentPermutations.forEach((components) => {
-//       output(level, `trying ${components.map((comp)=>{return comp.material})}`);
-//       components.forEach((component) => {
-//         const componentQtyNeeded = useInventory(inventory, component.material, component.qty, level)
-//         if (componentQtyNeeded) {
-//           const minReact = calculateMinimumOreReactions(reactions, component.material, componentQtyNeeded, level + 1);
-//           minTotal += minReact.oreNeeded;
-//           minReactions.push(react);
-//           combineInventory(inventory, minReact.left);
-//         }
-//       });
-//     });
-
-//     if (minOreReq == null || minTotal < minOreReq) {
-//       minOreReq = minTotal;
-//       minOreReactions = JSON.parse(JSON.stringify(minReactions));
-//       minInv = JSON.parse(JSON.stringify(inventory));
-//     }
-//   })
-
-//   output(level, `Need ${minOreReq} for ${targetQty} of ${targetMaterial}`);
-//   output(level, `Left: ${JSON.stringify(minInv)}`);
-//   return {
-//     oreNeeded: minOreReq,
-//     reactions: minOreReactions,
-//     left: minInv,
-//   }
-// }
 
 const performReaction = (oldInventory, reaction) => {
-  // todo
+  const newInv = JSON.parse(JSON.stringify(oldInventory));
+  for (let i = 0; i < reaction.components.length; i++) {
+    if (!addToInventory(newInv, reaction.components[i].material, -reaction.components[i].qty)) {
+      return null;
+    }
+  }
+
+  addToInventory(newInv, reaction.product.material, reaction.product.qty);
+
+  // console.log(`trying reaction: ${JSON.stringify(reaction)} (inv was: ${JSON.stringify(oldInventory)}, now ${JSON.stringify(newInv)}`);
+  return newInv;
 }
 
-const getValidReactions = (reactions, inv) => {
-  return reactions.filter((reaction) => {
-    const missingComponent = reaction.components.find((component) => {
-      return !haveInventory(inv, component.material, component.qty);
-    });
-
-    return !missingComponent;
-  });
+const haveInventory = (inv, material, qty) => {
+  // console.log(`inv[${material}]:${inv[material]} >= ${qty} is ${inv[material] >= qty}`);
+  return inv[material] >= qty;
 }
 
-const canMake = (reactions, targetMaterial, targetQty, parentInventory) => {
-  if (haveInventory(parentInventory, targetMaterial, targetQty) {
+const canMake = (reactions, targetMaterial, targetQty, parentInventory, level = 0) => {
+  // console.log(`@${level} Trying to make ${targetQty} of ${targetMaterial} with ${JSON.stringify(parentInventory)}`);
+  if (haveInventory(parentInventory, targetMaterial, targetQty)) {
     return true;
   }
 
-  let inv = JSON.parse(JSON.stringify(parentInventory));
-  const possibleReactons = getValidReactions(reactions, inventory);
+  for (let i = 0; i < reactions.length; i++) {
+    const newInv = performReaction(parentInventory, reactions[i]);
+    if (newInv && canMake(reactions, targetMaterial, targetQty, newInv, level + 1)) {
+      return true;
+    }
+  }
 
-  //todo try each then recurse
-  // return false if none succeed
   return false;
-
 }
 
 const findMinimumOreNeeded = (reactions, targetMaterial, targetQty) => {
   let oreUsed = 0;
 
   while (true) {
-    if (canMake(reactions, targetMaterial, targetQty, [{material: "ORE", qty: oreUsed}])) {
+    console.log(`Trying with ${oreUsed} ORE`);
+
+    let baseInv = {};
+    baseInv["ORE"] = oreUsed;
+
+    if (canMake(reactions, targetMaterial, targetQty, baseInv)) {
       return oreUsed
     }
 
@@ -166,15 +83,10 @@ const findMinimumOreNeeded = (reactions, targetMaterial, targetQty) => {
 const run = () => {
   const reactions = readStringArrayFromFile("./input/day14.txt", "\n").map(parseReaction);
   // console.log(JSON.stringify(reactions, null, 2));
-  // const reactionsByProduct = arrangeReactions(reactions);
-  // console.log(JSON.stringify(reactionsByProduct, null, 2));
 
   const requirements = findMinimumOreNeeded(reactions, "FUEL", 1);
 
-
-  // const minimumOreReactions = calculateMinimumOreReactions(reactionsByProduct, "FUEL", 1, 0);
-  // console.log(JSON.stringify(minimumOreReactions, null, 2));
-  console.log(`ORE NEEDED ${minimumOreReactions.oreNeeded}`);
+  console.log(`ORE NEEDED: ${requirements}`);
 }
 
 module.exports = {run}
